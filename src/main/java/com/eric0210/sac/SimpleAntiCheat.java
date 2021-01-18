26f3b0e25ba8ec1e4e31408fadbcfed92f05fbc5
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2021-01-19 04:59:57 +0900

    기초공사 #2

---
+++ src/main/java/com/eric0210/sac/SimpleAntiCheat.java
@@ -0,0 +1,13 @@
+package com.eric0210.sac;
+
+import java.util.logging.Logger;
+
+public final class SimpleAntiCheat
+{
+	private static final Logger LOGGER = Logger.getLogger(SimpleAntiCheat.class.getSimpleName());
+
+	public void info(final String message)
+	{
+		LOGGER.info(message);
+	}
+}
--- src/main/java/com/eric0210/sac/SimpleAntiCheatPlugin.java
+++ src/main/java/com/eric0210/sac/SimpleAntiCheatPlugin.java
@@ -1,7 +1,34 @@
 package com.eric0210.sac;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SimpleAntiCheatPlugin extends JavaPlugin
+public final class SimpleAntiCheatPlugin extends JavaPlugin
 {
+	public SimpleAntiCheatPlugin()
+	{
+	}
+
+	@Override
+	public void onLoad()
+	{
+	}
+
+	@Override
+	public void onEnable()
+	{
+		// TODO: 이벤트 핸들러 등록
+	}
+
+	@Override
+	public void onDisable()
+	{
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		return false;
+	}
 }
